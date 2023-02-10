aab15e3aa19275313c85575d342b385390617976
AndiS82 <19schneiderandreas82@gmail.com>
2023-02-10 10:20:49 +0100

    netlify

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -6,28 +6,26 @@ import { Navigate } from "react-router-dom";
 
 
 function SplashScreen() {
-    {
-        const [goToOnboarding, setGoToOnboarding] = useState(false);
+    const [goToOnboarding, setGoToOnboarding] = useState(false);
 
-        setTimeout(function () {
-            setGoToOnboarding(true);
-        }, 5000)
+    setTimeout(function () {
+        setGoToOnboarding(true);
+    }, 5000)
 
-        if (goToOnboarding === false) {
-            return (
-                <>
-                    <Link to={"/onboarding"}>
-                        <div>
-                            <img alt="" src={Icon}></img><br />
-                            <img alt="" src={ShopVector}></img>
-                            <p>Your Shopping Solution</p>
-                        </div>
-                    </Link>
-                </>
-            )
-        } else {
-            return <Navigate to="/onboarding" />
-        }
+    if (goToOnboarding === false) {
+        return (
+            <>
+                <Link to={"/onboarding"}>
+                    <div>
+                        <img alt="" src={Icon}></img><br />
+                        <img alt="" src={ShopVector}></img>
+                        <p>Your Shopping Solution</p>
+                    </div>
+                </Link>
+            </>
+        )
+    } else {
+        return <Navigate to="/onboarding" />
     }
 }
 
